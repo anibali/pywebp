17f8d71aad0cfc822720e7d88d6cc04a726a8073
felix <madwang@tencent.com>
2019-10-16 17:34:31 +0800

    upgrade to libwebp/1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webp',
-    version='0.1.0a14',
+    version='0.1.15',
     url='https://github.com/anibali/pywebp',
     packages=find_packages(include=['webp', 'webp.*', 'webp_build']),
     package_data={'webp_build': ['*.h', '*.c']},
@@ -15,7 +15,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     test_suite='tests',
-    setup_requires=['cffi>=1.0.0', 'conan>=1.8.0', 'importlib_resources>=1.0.0'],
+    setup_requires=['cffi>=1.0.3', 'conan>=1.8.0', 'importlib_resources>=1.0.0'],
     cffi_modules=['webp_build/builder.py:ffibuilder'],
     install_requires=['cffi>=1.0.0', 'Pillow>=4.0.0', 'numpy>=1.0.0'],
     classifiers=[
