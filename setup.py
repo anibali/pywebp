fd1dc9e53d6b49bd5057a8553fb2392e13ffc3b9
Aiden Nibali <dismaldenizen@gmail.com>
2020-06-09 19:48:02 +1000

    v0.1.0a17

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webp',
-    version='0.1.0a16',
+    version='0.1.0a17',
     url='https://github.com/anibali/pywebp',
     packages=find_packages(include=['webp', 'webp.*', 'webp_build']),
     package_data={'webp_build': ['*.h', '*.c']},
