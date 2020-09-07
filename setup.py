881d4c518e06d7ce5794894b8dcf407933419e6d
Aiden Nibali <dismaldenizen@gmail.com>
2020-09-08 09:14:29 +1000

    v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webp',
-    version='0.1.0a17',
+    version='0.1.0',
     url='https://github.com/anibali/pywebp',
     packages=find_packages(include=['webp', 'webp.*', 'webp_build']),
     package_data={'webp_build': ['*.h', '*.c']},
@@ -20,7 +20,6 @@ setup(
     cffi_modules=['webp_build/builder.py:ffibuilder'],
     install_requires=['cffi>=1.0.0', 'Pillow>=4.0.0', 'numpy>=1.0.0'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Topic :: Multimedia :: Graphics :: Graphics Conversion',
         'License :: OSI Approved :: MIT License',
     ]
