d0cb7bdaf20027371291201b4583ecf25490b54c
Aiden Nibali <dismaldenizen@gmail.com>
2021-09-11 09:28:45 +1000

    Move setup options to setup.cfg

--- setup.py
+++ setup.py
@@ -1,26 +1,8 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    name='webp',
-    version='0.1.3',
-    url='https://github.com/anibali/pywebp',
-    packages=find_packages(include=['webp', 'webp.*', 'webp_build']),
-    package_data={'webp_build': ['*.h', '*.c']},
-    author='Aiden Nibali',
-    description='Python bindings for WebP',
-    license='MIT',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    zip_safe=True,
-    test_suite='tests',
-    setup_requires=['wheel', 'cffi>=1.0.0', 'conan>=1.8.0', 'importlib_resources>=1.0.0'],
     cffi_modules=['webp_build/builder.py:ffibuilder'],
-    install_requires=['cffi>=1.0.0', 'Pillow>=4.0.0', 'numpy>=1.0.0'],
-    classifiers=[
-        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
-        'License :: OSI Approved :: MIT License',
-    ]
 )
