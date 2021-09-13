d8d2746ea80b7fd7f0d586b6c9e40c2f59cb3781
Aiden Nibali <dismaldenizen@gmail.com>
2021-09-13 16:36:49 +1000

    Improve support for 32-bit Python
    When 32-bit Python is detected on a 64-bit host, override Conan settings
    so that 32-bit libwebp is used.

--- webp_build/builder.py
+++ webp_build/builder.py
@@ -13,7 +13,12 @@ conan, _, _ = conan_api.ConanAPIV1.factory()
 
 # Use Conan to install libwebp
 with tempfile.TemporaryDirectory() as tmp_dir:
-    conan.install(path=getcwd(), cwd=tmp_dir)
+    settings = []
+    print(f'platform.architecture: {platform.architecture()}')
+    print(f'platform.machine: {platform.machine()}')
+    if platform.architecture()[0] == '32bit' and platform.machine().lower() in {'amd64', 'x86_64', 'x64'}:
+        settings.append('arch=x86')
+    conan.install(path=getcwd(), cwd=tmp_dir, settings=settings, build=['missing'])
     with open(path.join(tmp_dir, 'conanbuildinfo.json'), 'r') as f:
         conan_info = json.load(f)
 
