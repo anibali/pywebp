ca44adedc189c941c7f7f531efdc6bf01e58a966
laggykiller <61652821+laggykiller@users.noreply.github.com>
2023-09-07 07:26:12 +0800

    Fix macOS arm64 wheel building & Add wheel building for all architectures supported by cibuildwheel (#48)
    * Fix macOS arm wheel building
    * Build for all archs supported by cibuildwheel
    * Remove CMake as conan dep

--- webp_build/builder.py
+++ webp_build/builder.py
@@ -2,7 +2,7 @@ import json
 import platform
 import tempfile
 from importlib.resources import read_text
-from os import path, getcwd
+from os import path, getcwd, getenv
 
 from cffi import FFI
 from conans.client import conan_api
@@ -12,19 +12,36 @@ import webp_build
 conan, _, _ = conan_api.ConanAPIV1.factory()
 
 # Use Conan to install libwebp
+settings = []
+print(f'platform.architecture: {platform.architecture()}')
+print(f'platform.machine: {platform.machine()}')
+if platform.architecture()[0] == '32bit' and platform.machine().lower() in {'amd64', 'x86_64', 'x64', 'i686'}:
+    settings.append('arch=x86')
+if getenv('CIBW_ARCHS_MACOS') == 'arm64':
+    # https://blog.conan.io/2021/09/21/m1.html
+    settings.append('os=Macos')
+    settings.append('arch=armv8')
+    settings.append('compiler=apple-clang')
+    settings.append('compiler.version=11.0')
+    settings.append('compiler.libcxx=libc++')
+elif getenv('CIBW_ARCHS_WINDOWS') == 'ARM64':
+    settings.append('os=Windows')
+    settings.append('arch=armv8')
+if getenv('CIBW_BUILD') and 'musllinux' in getenv('CIBW_BUILD'):
+    build_policy = ['always']
+else:
+    build_policy = ['missing']
+
 with tempfile.TemporaryDirectory() as tmp_dir:
-    settings = []
-    print(f'platform.architecture: {platform.architecture()}')
-    print(f'platform.machine: {platform.machine()}')
-    if platform.architecture()[0] == '32bit' and platform.machine().lower() in {'amd64', 'x86_64', 'x64'}:
-        settings.append('arch=x86')
-    conan.install(path=getcwd(), cwd=tmp_dir, settings=settings, build=['missing'],
-                  profile_names=[path.abspath('conan_profile')])
+    conan.install(path=getcwd(), cwd=tmp_dir, settings=settings, build=build_policy)
     with open(path.join(tmp_dir, 'conanbuildinfo.json'), 'r') as f:
         conan_info = json.load(f)
 
+print(conan_info)
+
 # Find header files and libraries in libwebp
 extra_objects = []
+extra_compile_args = []
 include_dirs = []
 libraries = []
 for dep in conan_info['dependencies']:
@@ -42,12 +59,22 @@ for dep in conan_info['dependencies']:
     for include_path in dep['include_paths']:
         include_dirs.append(include_path)
 
+if getenv('CIBW_ARCHS_MACOS') == 'arm64':
+    extra_compile_args.append('--target=arm64-apple-macos11')
+
+print('CFFI arguments:')
+print(f'{extra_objects = }')
+print(f'{extra_compile_args = }')
+print(f'{include_dirs = }')
+print(f'{libraries = }')
+
 # Specify C sources to be built by CFFI
 ffibuilder = FFI()
 ffibuilder.set_source(
     '_webp',
     read_text(webp_build, 'source.c'),
     extra_objects=extra_objects,
+    extra_compile_args=extra_compile_args,
     include_dirs=include_dirs,
     libraries=libraries,
 )
