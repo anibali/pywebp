fd990987b6700c063fe0c058afc7853fd6da1a3d
laggykiller <chaudominic2@gmail.com>
2023-09-07 02:59:05 +0800

    Properly remove conan_profile

--- webp_build/builder.py
+++ webp_build/builder.py
@@ -29,8 +29,7 @@ elif getenv('CIBW_ARCHS_WINDOWS') == 'ARM64':
     settings.append('arch=armv8')
 
 with tempfile.TemporaryDirectory() as tmp_dir:
-    conan.install(path=getcwd(), cwd=tmp_dir, settings=settings, build=['missing'],
-                  profile_names=[path.abspath('conan_profile')])
+    conan.install(path=getcwd(), cwd=tmp_dir, settings=settings, build=['missing'])
     with open(path.join(tmp_dir, 'conanbuildinfo.json'), 'r') as f:
         conan_info = json.load(f)
 
