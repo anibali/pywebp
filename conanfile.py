6fb2241228c40793d413de1207861a2c59aab44f
laggykiller <chaudominic2@gmail.com>
2023-09-09 13:00:19 +0800

    Only set cmake ass requirements if cmake absent

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+import shutil
 
 class LibwebpRecipe(ConanFile):
     def requirements(self):
@@ -6,4 +7,5 @@ class LibwebpRecipe(ConanFile):
 
     def build_requirements(self):
         # if self.settings.arch in ("x86_64", "armv8"):
-        self.tool_requires("cmake/[>=3.5]")
+        if not shutil.which('cmake'):
+            self.tool_requires("cmake/[>=3.5]")
